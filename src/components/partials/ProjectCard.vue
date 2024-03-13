acd2642b2062a8853f02ff7d615f782e488a0ba6
gabrielebottari <gabriele.bottari1@gmail.com>
2024-03-13 19:06:21 +0100

    ADD pagination BONUS END exercise

--- src/components/partials/ProjectCard.vue
+++ src/components/partials/ProjectCard.vue
@@ -2,15 +2,12 @@
 export default {
   name: 'ProjectCard',
   props: {
-    title: String,
-    description: String,
-    image: String,
-    date: String,
+    project: Object,
   },
   computed: {
     projectImageSrc() {
       // Assumi che `image` sia un URL completo o un percorso relativo da /storage
-      return this.image.startsWith('http') ? this.image : `/storage/${this.image}`;
+      return this.project.image.startsWith('http') ? this.project.image : `/storage/${this.project.image}`;
     }
   }
 }
@@ -19,11 +16,16 @@ export default {
 <template>
 <div class="col-md-4 p-3">
   <div class="card mb-3">
-    <img v-if="image" :src="projectImageSrc" :alt="`Immagine di ${title}`" class="card-img-top">
-    <h5 class="card-title p-2">{{ title }}</h5>
+    <img v-if="project.image" :src="projectImageSrc" :alt="`Immagine di ${project.title}`" class="card-img-top">
     <div class="card-body m-2">
-      <p class="card-text">{{ description }}</p>
-      <p class="card-text"><small class="text-muted">{{ date }}</small></p>
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p>Type: {{ project.type.name }}</p>
+        <h6>Tecnologie:</h6>
+        <ul>
+        <li v-for="tech in project.technologies" :key="tech.id">{{ tech.name }}</li>
+        </ul>
+        <p class="card-text">{{ project.description }}</p>
+        <p class="card-text"><small class="text-muted">{{ project.date }}</small></p>
     </div>
   </div>
 </div>
